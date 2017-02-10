bed52c48c33233865910b0a60279dfc785666a7a
John Kelly <john.j.kelly94@gmail.com>
2017-02-10 17:53:14 +0000

    removed files

--- tests/frontend/conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-	seleniumServerJar: '../../frontend/node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar',
-	specs: ['./functional/**/*-test.js'],
-	seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-	capabilities: {
-		'browserName': 'chrome'
-	},
-	baseUrl: 'http://localhost:8888/#!/'
-};
