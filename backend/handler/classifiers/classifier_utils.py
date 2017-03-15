1f8c6ca311096d89444db9f7a78c29e7c52f274a
John Kelly <john.j.kelly94@gmail.com>
2017-03-15 11:54:46 +0000

    modualised classifiers

--- backend/handler/classifiers/classifier_utils.py
+++ backend/handler/classifiers/classifier_utils.py
@@ -1,8 +1,18 @@
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import LinearSVC
+from handler.classifiers.vote_classifier import VoteClassifier
+
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score
 
 from nltk.classify.scikitlearn import SklearnClassifier
+from nltk.classify.util import accuracy
+
+from handler.slack import send_completed_message
+from random import shuffle
+from typing import Union
+import numpy as np
 
 import os.path
 import pickle
@@ -36,3 +46,52 @@ def load_classifier(name):
     else:
         classifier = SklearnClassifier(classifiers[name])
     return classifier
+
+
+def test_classifier(
+    name: str,
+    classifier: VoteClassifier,
+    data: Union[np.array, list]
+):
+    percentage = (accuracy(classifier, data) * 100)
+    print(name, "accuracy percent:", percentage)
+    if name is "Final":
+        send_completed_message(percentage)
+
+
+def train_classifiers(data: Union[np.array, list]):
+    for name in get_classifier_names():
+        classifier = load_classifier(name)
+        print('Training', name, '...')
+        classifier.train(data)
+        with open(
+            PICKLE_PATH + name + '_classifier.pickle',
+            'wb'
+        ) as ph:
+            pickle.dump(classifier, ph)
+
+
+def validate_classifiers(data: Union[np.array, list]):
+    kfold = model_selection.KFold(n_splits=10, shuffle=True, random_state=7)
+    for name in get_classifier_names():
+        classifier = load_classifier(name)
+        print('Training and testing', name, '...')
+        for traincv, testcv in kfold.split(data):
+            classifier.train(data[traincv])
+            test_classifier(name, classifier, data[testcv])
+        with open(
+            PICKLE_PATH + name + '_classifier.pickle',
+            'wb'
+        ) as ph:
+            pickle.dump(classifier, ph)
+
+
+def voted_classifier(data: Union[np.array, list]):
+    new_models = []
+    for name in get_classifier_names():
+        classifier = load_classifier(name)
+        new_models.append(classifier)
+
+    print('Voting on classifier prediction...')
+    classifier = VoteClassifier(new_models)
+    test_classifier("Final", classifier, data)
--- backend/train_classifier.py
+++ backend/train_classifier.py
@@ -1,74 +1,37 @@
-import pickle
 import numpy as np
 
-from sklearn import model_selection
-from nltk.classify.util import accuracy
-from math import floor
+from sklearn.model_selection import train_test_split
 
-from handler.slack import send_completed_message
 from handler.classifiers.structure_data import getData
-from handler.classifiers.vote_classifier import VoteClassifier
+
 from handler.classifiers.classifier_utils import get_classifier_names
 from handler.classifiers.classifier_utils import load_classifier
 
+from handler.classifiers.classifier_utils import validate_classifiers
+from handler.classifiers.classifier_utils import test_classifier
+from handler.classifiers.classifier_utils import voted_classifier
+
+DIR = 'handler/classifiers/'
+
+NEW_SENTIMENT_FOLDER = DIR + 'sentiment_files' + '/'
 
 FILE_NUMBER = 1
-DIR = 'handler/classifiers/'
-NEW_SENTIMENT_FOLDER = DIR + 'sentiment_files/'
 
+POSFILE = NEW_SENTIMENT_FOLDER + 'neg' + str(FILE_NUMBER) + '.txt'
+NEGFILE = NEW_SENTIMENT_FOLDER + 'pos' + str(FILE_NUMBER) + '.txt'
 
-POSFILE = NEW_SENTIMENT_FOLDER + 'pos' + str(FILE_NUMBER) + '.txt'
-NEGFILE = NEW_SENTIMENT_FOLDER + 'neg' + str(FILE_NUMBER) + '.txt'
-
-PICKLE_PATH = DIR + 'pickle/'
 
 data = getData(positive=POSFILE, negative=NEGFILE)
 
 
-def test_classifier(name, classifier, data):
-    percentage = ((accuracy(classifier, data) * 100))
-    print(name, "accuracy percent:", percentage)
-    if name is "Final":
-        send_completed_message(percentage)
-
-
-def train_classifiers(data):
-    for name in get_classifier_names():
-        classifier = load_classifier(name)
-        print('Training', name, '...')
-        classifier.train(data)
-        with open(
-            PICKLE_PATH + name + '_classifier.pickle',
-            'wb'
-        ) as ph:
-            pickle.dump(classifier, ph)
-
-
-def validate_classifiers(data):
-    kfold = model_selection.KFold(n_splits=10, shuffle=True, random_state=7)
-    for name in get_classifier_names():
-        classifier = load_classifier(name)
-        print('Training and testing', name, '...')
-        for traincv, testcv in kfold.split(data):
-            classifier.train(data[traincv])
-            test_classifier(name, classifier, data[testcv])
-        with open(
-            PICKLE_PATH + name + '_classifier.pickle',
-            'wb'
-        ) as ph:
-            pickle.dump(classifier, ph)
-
-
 print('Prepare training data...')
-cv_test_split = int(floor(len(data) * 0.5))
-cross_validation_set = np.array(data[:cv_test_split])
-testing_set = np.array(data[cv_test_split:])
+validation_size = 0.5
+seed = 7
+train, test = train_test_split(
+    data,
+    test_size=validation_size,
+    random_state=seed
+)
 
-new_models = []
-for name in get_classifier_names():
-    classifier = load_classifier(name)
-    new_models.append(classifier)
-
-print('Voting on classifier prediction...')
-voted_classifier = VoteClassifier(new_models)
-test_classifier("Final", voted_classifier, testing_set)
+validate_classifiers(np.array(train))
+voted_classifier(np.array(test))
